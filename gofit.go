6a8efb89e5e29b19ca4cb8cf2984eaa6ad5b405a
Tim Sullivan <tsullivan@timatooth.com>
2017-05-20 01:44:45 +1200

    Move api code to seperate go package

--- gofit.go
+++ gofit.go
@@ -3,193 +3,36 @@ package main
 import (
   "fmt"
   "log"
-  "bytes"
   "os"
-  "strings"
   "time"
-  "io/ioutil"
-  "encoding/json"
-  "encoding/base64"
   "net/http"
-  "net/url"
-  "text/template"
   "github.com/influxdata/influxdb/client/v2"
+  "github.com/timatooth/gofit/fitbitapi"
 )
-
-type FitbitApi struct {
-  ClientId, ClientSecret, RedirectUri, AuthorizeUri, Scopes string
-  Auth FitbitAuth
-}
-
-type FitbitAuth struct {
-  AccessToken string `json:"access_token"`
-  RefreshToken string `json:"refresh_token"`
-  UserId string `json:"user_id"`
-  TokenType string `json:"token_type"`
-  Scope string `json:"scope"`
-}
 
 const (
-  MyDB = "mydb"
-  username = ""
-  password = ""
+  InfluxDatabaseName = "fitbit"
+  InfluxUsername = ""
+  InfluxPassword = ""
 )
 
-func NewFitBitApi(ClientId string, ClientSecret string, RedirectUri string) FitbitApi {
-  authorizeTmpl := "response_type=code&client_id={{.ClientId}}&redirect_uri={{.RedirectUri}}&scope={{.Scopes}}&expires_in=604800"
-  tmpl, err := template.New("test").Parse(authorizeTmpl)
-  api := FitbitApi{}
-  api.ClientId = ClientId
-  api.ClientSecret = ClientSecret
-  api.RedirectUri = RedirectUri
-  api.Scopes = "profile settings location heartrate activity weight sleep nutrition"
-  if err != nil { panic(err) }
-  var authorizeBuf bytes.Buffer
-  err = tmpl.Execute(&authorizeBuf, api)
-  if err != nil { panic(err) }
-  api.AuthorizeUri = "https://www.fitbit.com/oauth2/authorize?" + url.PathEscape(authorizeBuf.String())
-  return api
-}
 
-func (api *FitbitApi) EncodeBasicAuth() string{
-  authstring := api.ClientId + ":" + api.ClientSecret
-  return base64.StdEncoding.EncodeToString([]byte(authstring))
-}
-
-func (api *FitbitApi) GetProfile() string{
-  req, _ := http.NewRequest("GET", "https://api.fitbit.com/1/user/-/profile.json", nil)
-  req.Header.Set("Authorization", "Bearer " + api.Auth.AccessToken)
-  res, _ := http.DefaultClient.Do(req)
-  profiledata, _:= ioutil.ReadAll(res.Body)
-  res.Body.Close()
-  return string(profiledata)
-}
-
-type ActivitySteps struct {
-  Steps []DataPoint `json:"activities-steps"`
-}
-
-type DataPoint struct {
-  Time string `json:"dateTime"`
-  Value string `json:"value"`
-}
-
-func (api *FitbitApi) GetActivitySteps() ActivitySteps{
-  req, _ := http.NewRequest("GET", "https://api.fitbit.com/1/user/-/activities/steps/date/today/1y.json", nil)
-  req.Header.Set("Authorization", "Bearer " + api.Auth.AccessToken)
-  res, _ := http.DefaultClient.Do(req)
-  var activitySteps ActivitySteps
-  decoder := json.NewDecoder(res.Body)
-  decerr := decoder.Decode(&activitySteps)
-  if decerr != nil { panic(decerr) }
-  res.Body.Close()
-  return activitySteps
-}
-
-type HeartDataPoint struct {
-  Date string `json:"dateTime"`
-  Value HeartDataValue `json:"value"`
-}
-
-type HeartDataValue struct {
-  RestingHeartRate int `json:"restingHeartRate"`
-}
-
-type ActivityHeart struct {
-  HeartData []HeartDataPoint `json:"activities-heart"`
-}
-
-func (api *FitbitApi) GetRestingHeartrate() ActivityHeart{
-  req, _ := http.NewRequest("GET", "https://api.fitbit.com/1/user/-/activities/heart/date/today/1y.json", nil)
-  req.Header.Set("Authorization", "Bearer " + api.Auth.AccessToken)
-  res, _ := http.DefaultClient.Do(req)
-  var activityHeart ActivityHeart
-  decoder := json.NewDecoder(res.Body)
-  decerr := decoder.Decode(&activityHeart)
-  if decerr != nil { panic(decerr) }
-  res.Body.Close()
-  return activityHeart
-}
-
-type HeartateIntradayPoint struct {
-  Time string `json:"time"`
-  Value int `json:"value"`
-}
-
-type HeartIntraday struct {
-  Dataset []HeartateIntradayPoint `json:"dataset"`
-  DatasetInterval int `json:"DatasetInterval"`
-  DatasetType string `json:"datasetType"`
-}
-type ActivityHeartSeries struct {
-  HeartData []HeartDataPoint `json:"activities-heart"`
-  HeartIntraday HeartIntraday `json:"activities-heart-intraday"`
-}
-
-type NormalisedHeartPoint struct {
-  Timestamp time.Time
-  Value int
-}
-
-func (series *ActivityHeartSeries) GetNormalisedSeries(timezone string) []NormalisedHeartPoint {
-  loc, _ := time.LoadLocation(timezone)
-  var points []NormalisedHeartPoint
-  format := "2006-01-02 15:04:05"
-  for _, datapoint := range series.HeartIntraday.Dataset {
-    timestamp, e := time.ParseInLocation(format, series.HeartData[0].Date + " " + datapoint.Time, loc)
-    if(e != nil){panic(e)}
-    points = append(points, NormalisedHeartPoint{Timestamp: timestamp, Value: datapoint.Value})
-  }
-
-  return points
-}
-
-func (api *FitbitApi) GetHeartrateTimeSeries(date string) ActivityHeartSeries {
-  req, _ := http.NewRequest("GET", "https://api.fitbit.com/1/user/-/activities/heart/date/"+date+"/1d/1sec.json", nil)
-  req.Header.Set("Authorization", "Bearer " + api.Auth.AccessToken)
-  res, _ := http.DefaultClient.Do(req)
-  var series ActivityHeartSeries
-  dec := json.NewDecoder(res.Body)
-  decerr := dec.Decode(&series)
-  if(decerr != nil) { panic(decerr) }
-  return series
-}
-
-func (api *FitbitApi) LoadAccessToken(code string){
-  form := url.Values{}
-  form.Add("clientId", api.ClientId)
-  form.Add("grant_type", "authorization_code")
-  form.Add("redirect_uri", api.RedirectUri)
-  form.Add("code", code)
-  req, err := http.NewRequest("POST", "https://api.fitbit.com/oauth2/token", strings.NewReader(form.Encode()))
-  req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-  req.Header.Set("Authorization", "Basic " + api.EncodeBasicAuth())
-  res, err := http.DefaultClient.Do(req)
-  if err != nil { panic(err) }
-  decoder := json.NewDecoder(res.Body)
-
-  var auth FitbitAuth
-  decerr := decoder.Decode(&auth)
-  if decerr != nil { panic(decerr) }
-  res.Body.Close()
-  api.Auth = auth
-}
-
-func (api *FitbitApi) loadInfluxData(){
+func loadInfluxData(api *fitbitapi.Api){
   fmt.Println("Loading step data into influxdb...")
   activitySteps := api.GetActivitySteps()
 
   c, err := client.NewHTTPClient(client.HTTPConfig{
     Addr:     "http://localhost:8086",
-    Username: username,
-    Password: password,
+    Username: InfluxUsername,
+    Password: InfluxPassword,
   })
+
   if err != nil {
     log.Fatal(err)
   }
 
   bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-    Database:  MyDB,
+    Database:  InfluxDatabaseName,
     Precision: "s",
   })
   if err != nil {
@@ -222,7 +65,7 @@ func (api *FitbitApi) loadInfluxData(){
   activityHeart := api.GetRestingHeartrate()
 
   bp, err2 := client.NewBatchPoints(client.BatchPointsConfig{
-    Database:  MyDB,
+    Database:  InfluxDatabaseName,
     Precision: "s",
   })
   if err2 != nil {
@@ -260,7 +103,7 @@ func (api *FitbitApi) loadInfluxData(){
     series := api.GetHeartrateTimeSeries(dateString)
 
     bp, _ = client.NewBatchPoints(client.BatchPointsConfig{
-      Database:  MyDB,
+      Database:  InfluxDatabaseName,
       Precision: "s",
     })
 
@@ -286,14 +129,14 @@ func (api *FitbitApi) loadInfluxData(){
 
 func main() {
   mux := http.NewServeMux()
-  api := NewFitBitApi(os.Getenv("FITBIT_CLIENT_ID"), os.Getenv("FITBIT_CLIENT_SECRET"), "http://localhost:4000/auth")
+  api := fitbitapi.New(os.Getenv("FITBIT_CLIENT_ID"), os.Getenv("FITBIT_CLIENT_SECRET"), "http://localhost:4000/auth")
 
   mux.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
     code := r.URL.Query()["code"][0]
     api.LoadAccessToken(code)
     fmt.Fprintf(w, api.GetProfile())
 
-    api.loadInfluxData()
+    loadInfluxData(&api)
   })
 
   mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
